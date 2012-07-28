7b5ca0f1dde3d7b68eb4125a570c9dbb2aa08196
Tassandar <tassandar@163.com>
2012-07-28 14:11:34 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Xuehua::Application.config.secret_token = '59541e933b19ab9dd2cefabd4e9cd3474db13117bb6d131a09ce6de14e388634b81bdd71b0124816ec3d89d0a1eef76b2088c0b20f7491a806c707b58fbc1ad3'

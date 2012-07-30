f2f1b8025af0948707e33344ddadc00db2cd7177
Mato <mato@mato-VirtualBox.(none)>
2012-07-30 15:06:02 +0800

    7301500

--- app/assets/javascripts/kkk.js
+++ app/assets/javascripts/kkk.js
@@ -5,6 +5,28 @@
 
 
 $(function(){
+	
+//email input
+	$("#user_email").mouseover(function(){
+		
+		var val = $(this).val();
+		
+		if(val == "邮箱"){
+			if(this)
+			$(this).val("").focus();
+		}
+		
+	}).blur(function(){
+		var val = $(this).val();
+		
+		if(val == ""){
+			if(this)
+			$(this).val("邮箱");
+		}
+	});
+
+	
+//click checkbox
 	$(".check").click(function(){
 		$(this).toggleClass("checked");
 		var checkObj = $(this).children("input")[0];

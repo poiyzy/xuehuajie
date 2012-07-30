b9438be0a13aed47af5fd4dc432f1646848b9d23
Mato <mato@mato-VirtualBox.(none)>
2012-07-30 11:50:30 +0800

    07301200

--- app/assets/javascripts/kkk.js
+++ app/assets/javascripts/kkk.js
@@ -5,6 +5,12 @@
 
 
 $(function(){
-	
+	$(".check").click(function(){
+		$(this).toggleClass("checked");
+		var checkObj = $(this).children("input")[0];
+		checkObj.checked=!checkObj.checked;
+	});
+
+		
 	
 })
